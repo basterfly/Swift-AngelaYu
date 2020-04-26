e523d07acf13c656220cf60d9758214cfc282650
Yegor Kozlovskiy <basterfly@gmail.com>
2020-04-26 15:53:44 +0300

    Home work finished. Magic Ball shows a random answer.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    override func viewDidLoad() {
+        imageView.image = ballArray[2]
+    }
+    
+    @IBAction func askActionButton(_ sender: UIButton) {
+     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+        imageView.image = ballArray.randomElement()
+        
+    }
 
 
 
