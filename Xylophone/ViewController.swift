56d38769cc01afdb0520618ace02211e2d021dca
Yegor Kozlovskiy <basterfly@gmail.com>
2020-05-05 17:40:05 +0300

    Better than my version.         playSound(soundName: sender.currentTitle!)

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,36 +18,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        if sender.currentTitle == "C" {
-            playSound(soundName: "C")
-            print(sender.currentTitle as Any)
-        }
-        if sender.currentTitle == "D" {
-            playSound(soundName: "D")
-            print(sender.currentTitle!)
-        }
-        if sender.currentTitle == "E" {
-            playSound(soundName: "E")
-            print(sender.currentTitle!)
-        }
-        if sender.currentTitle == "F" {
-            playSound(soundName: "F")
-            print(sender.currentTitle)
-        }
-        if sender.currentTitle == "G" {
-            playSound(soundName: "G")
-            print(sender.currentTitle!)
-        }
-        if sender.currentTitle == "A" {
-            playSound(soundName: "A")
-            print(sender.currentTitle)
-        }
-        if sender.currentTitle == "B" {
-            playSound(soundName: "B")
-            print(sender.currentTitle)
-        }
         
-//        playSound(currentTitle: <#T##String#>)
+        playSound(soundName: sender.currentTitle!)
+        print(sender.currentTitle!)
     }
     
     func playSound(soundName: String) {
