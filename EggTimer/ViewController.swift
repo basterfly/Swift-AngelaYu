5ee22c44101d9c36c3b0e762f0873158e9cf7760
Yegor Kozlovskiy <basterfly@gmail.com>
2020-05-13 17:13:04 +0300

    made the sixtySecondsTimer() function with  for-in loop using sleep() function

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -15,7 +15,15 @@ class ViewController: UIViewController {
     @IBAction func softAction(_ sender: UIButton) {
         let hardness = sender.titleLabel!.text!
         print(eggTimes[hardness]!)
+        sixtySecondsTimer(multipliyer: eggTimes[hardness]!)
+        
     }
     
-
+    func sixtySecondsTimer(multipliyer: Int) {
+        let seconds = 60 * multipliyer
+        for i in (-1 * seconds)...(-1) {
+            print(-i)
+            sleep(1)
+        }
+    }
 }
