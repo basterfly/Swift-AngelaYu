fba9f177891771c56cb60a208be7c74f01c6cff1
Yegor Kozlovskiy <basterfly@gmail.com>
2020-05-25 13:41:40 +0300

    Made the new Question.swift file with Struct Question{}. Renewed list of questions with answers, code has been corrected.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,11 +16,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        ["Four + two is equal to Six?", "True"],
-        ["Five - Three is greater than One?", "True"],
-        ["Three + Eight is less than Ten?", "False"]
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
     ]
     var questionNumber = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,25 +40,35 @@ class ViewController: UIViewController {
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        timer.invalidate()
         let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[questionNumber][1]
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
+            sender.backgroundColor = UIColor.green
             print("Right!")
-            progressBar.progress += 0.1
+            progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
         } else {
+            sender.backgroundColor = UIColor.red
             print("Wrong!")
         }
         
         questionNumber += 1
-        if questionNumber == 3 {
+        if questionNumber == quiz.count - 1 {
             questionNumber = 0
         }
-        updateUi()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false, block: { (timer) in
+            self.updateUi()
+        })
     }
     
     func updateUi() {
-        questionLabel.text = quiz[questionNumber][0]
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        questionLabel.text = quiz[questionNumber].text
+        timer.invalidate()
+        
     }
 
 }
