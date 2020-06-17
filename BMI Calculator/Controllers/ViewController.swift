b478c094ab4184688e787a5a987a7a8634abcf9b
Yegor Kozlovskiy <basterfly@gmail.com>
2020-06-17 15:01:38 +0300

    Made action links to sliders and print value when sliding

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var height: Float = 0.0
+    @IBAction func heightSlider(_ sender: UISlider) {
+        height = sender.value
+        print(height)
+    }
+    @IBAction func weightSlider(_ sender: UISlider) {
+        print(sender.value.rounded())
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
