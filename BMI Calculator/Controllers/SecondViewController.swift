e96f0c1ccfed3c544480ae49544ebfbdf3643017
Yegor Kozlovskiy <basterfly@gmail.com>
2020-06-25 13:47:34 +0300

    Made the new file and class SecondViewController

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Yegor Kozlovskiy on 24.06.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
