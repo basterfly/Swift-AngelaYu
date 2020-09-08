4ca01b9bf7bc7c81435f07b67dbfaec1ca42c5c7
Egor <basterfly@gmail.com>
2020-09-08 13:28:24 +0300

    Made a group for Controllers. added ResultViewController file and linked IBOutlets and IBAction. #146 Video

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Yegor on 08.09.2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBAction func recalculateButton(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
