b352fbe57f71d4b05802a2c1638248cc70024209
Yegor Kozlovskiy <basterfly@gmail.com>
2020-11-15 17:07:38 +0200

    #154-158 Made background and color sets for light and dark mode. added background vector images and set them. WeatherViewController got the protocol UITextFieldDelegate for require. WeatherViewController was sat as Delegate for SearchTextField and now Go button on KB doing the same as search button.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { //обрабатывает нажатие GO/Return для поля textField
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            textField.placeholder = "Type your city name here"
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) { //введенный текст остается в поле с применением функции
+//        textField.text = "" //а так мы его обнулим как и без этого метода
+    }
 }
 
