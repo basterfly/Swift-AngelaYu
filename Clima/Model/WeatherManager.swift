061f9cbcf32e1c8bdd0880c9d627af3a6d93935d
YEgor <basterfly@gmail.com>
2020-11-18 00:21:15 +0200

    #160 Use the URLSession for Networking. func performRequest(urlString: String) : URL, URLSession, session.dataTask(with: url, completionHandler: (Data?: Response?: Error?:) -> Void)

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,11 +9,31 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?&units=metric&appid=04514790ff608722834c77770feb2ad6" //q=Kharkiv
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=04514790ff608722834c77770feb2ad6" //q=Kharkiv
 
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
         print(urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return //выйти из ф-ции т.к. есть ошибка
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: String.Encoding.utf8)
+            print(dataString)
+        }
     }
 
 }
