89f5cf8031a6cbb262005528b1c31733119d0ec8
YEgor <basterfly@gmail.com>
2020-12-01 14:28:45 +0200

    #163 New file WeatherModel with properties which values I got from API (WeatherData). Using Computed Properties. Corrected code with MVC.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-var weatherController = WeatherViewController()
-
 struct WeatherManager {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=04514790ff608722834c77770feb2ad6" //q=Kharkiv
 
@@ -40,37 +38,17 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
-//            weatherController.cityLabel.text = decodedData.name
-            print(decodedData.main.temp)
-//            weatherController.temperatureLabel.text = String(decodedData.main.temp)
-            print(decodedData.weather[0].description)
+            let name = decodedData.name
+            let temp = decodedData.main.temp
             let id = decodedData.weather[0].id
-            print(getConditionName(conditionId: id))
+            let description = decodedData.weather[0].description
+            
+            let weather = WeatherModel(cityName: name, conditionId: id, temperature: temp)
+            
+            print(weather.conditionName)
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
-    }
-    
-    func getConditionName(conditionId: Int) -> String {
-        switch conditionId {
-        case 200...232:
-            return "cloud.bolt"
-        case 300...321:
-            return "cloud.drizzle"
-        case 500...531:
-            return "cloud.rain"
-        case 600...622:
-            return "cloud.snow"
-        case 701...781:
-            return "cloud.fog"
-        case 800:
-            return "sun.max"
-        case 801...804:
-            return "cloud.bolt"
-        default:
-            return "cloud"
-        }
-        
     }
 }
